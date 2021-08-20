e64f230ec3792f4460f059299537247ff0d14d54
Zixuan Chen <remch183@outlook.com>
2021-08-20 23:16:42 +0800

    chore: HTMLPlugin

--- dist/main.js
+++
@@ -1,1 +0,0 @@
-(()=>{"use strict";const e=document.createElement("div");document.body.appendChild(e)})();
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 export { add } from './add';
 
 const div = document.createElement('div');
+div.innerHTML = 'Hello!';
 document.body.appendChild(div);
