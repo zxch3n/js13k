5de0b778485d9bda77ed6236d43e63be32f5cc30
Zixuan Chen <remch183@outlook.com>
2021-09-01 00:55:09 +0800

    fix: formatting & type error

--- src/gameObject.ts
+++ src/gameObject.ts
@@ -1,24 +1,24 @@
 import { LocalPosition, toGlobal } from './position';
 import { Sprite } from './sprite';
-export default abstract class GameObject{
-  sprite?:Sprite
-  isAlive=true  // if !isAlive Don't render
-  listeners: {[event: string]: {(event:GEvent): void}[]} = { all: [] };
+export default abstract class GameObject {
+  sprite?: Sprite;
+  isAlive = true; // if !isAlive Don't render
+  listeners: { [event: string]: { (event: GEvent): void }[] } = { all: [] };
 
   protected lastUpdated = +new Date();
 
-  addListener(eventName:string, listener:{(event:GEvent): void}):void{
-    if (!(eventName in this.listeners)){
+  addListener(eventName: string, listener: { (event: GEvent): void }): void {
+    if (!(eventName in this.listeners)) {
       // Not registered
-      this.listeners[eventName]=[];
+      this.listeners[eventName] = [];
     }
-    this.listeners[eventName].push(listener)
+    this.listeners[eventName].push(listener);
   }
 
-  emit(event:GEvent):void{
-    if (event.eventName in this.listeners){
-      for (const listener of this.listeners[event.eventName]){
-        listener(event)
+  emit(event: GEvent): void {
+    if (event.eventName in this.listeners) {
+      for (const listener of this.listeners[event.eventName]) {
+        listener(event);
       }
     }
   }
@@ -31,39 +31,38 @@ export default abstract class GameObject{
     this.sprite!.pos = toGlobal(pos);
   }
 
-  abstract update():void;
+  abstract update(): void;
 }
 
-export class GEvent{
+export class GEvent {
   constructor(
-    public eventName:string,
-    public target?:GameObject,
-    public extra?:Object
+    public eventName: string,
+    public target?: GameObject,
+    public extra?: Object,
   ) {}
-
 }
 
-export class ObjectPool{
-  private pool: GameObject[] = [];
-  protected factory:([])=>GameObject;
-  constructor(fac:([])=>GameObject){
+export class ObjectPool<T extends GameObject> {
+  private pool: T[] = [];
+  protected factory: ([]) => T;
+  constructor(fac: ([]) => T) {
     this.factory = fac;
   }
 
-  instantiate(initCall?:(item:GameObject)=>void, ...args:unknown[]):GameObject{
-    let item = this.findAliveItem()
-    if (!item){
+  instantiate(initCall?: (item: T) => void, ...args: unknown[]): T {
+    let item = this.findAliveItem();
+    if (!item) {
       item = this.factory(args);
-      this.pool.push(item)
+      this.pool.push(item);
     }
-    if(initCall){
+    if (initCall) {
       initCall(item);
     }
-    return item
+    return item;
   }
 
-  private findAliveItem():GameObject|undefined{
-    let items = this.pool.filter(x => !x.isAlive)
-    return items.pop()
+  private findAliveItem(): T | undefined {
+    let items = this.pool.filter((x) => !x.isAlive);
+    return items.pop();
   }
 }
